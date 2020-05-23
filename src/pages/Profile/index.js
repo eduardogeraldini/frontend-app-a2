55fa258d2c4e2b5b5662ccdcc235afa8910c3e0e
marlonx19 <marlon.englemam@gmail.com>
2020-05-23 16:08:04 -0300

    added feature to upload image from phone to backend

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -17,9 +17,22 @@ import * as Permissions from 'expo-permissions';
 import styles from "./styles";
 
 export default function Profile() {
-  const [avatar, setAvatar] = useState('');
+  const [avatar, setAvatar] = useState(null);
+  const [image, setImage] = useState(null);
 
   const navigation = useNavigation();
+
+
+  async function listAll(){
+    let apiUrl = 'http://192.168.15.13:3000/users';
+
+    let options = {
+      method: 'GET',
+    };
+
+    const res = await fetch(apiUrl, options);
+    console.log(res.json())
+  }
 
   async function _pickImage() {
     try {
@@ -31,6 +44,7 @@ export default function Profile() {
       });
       if (!result.cancelled) {
         setAvatar(result.uri)
+        _handleImagePicked(result)
       }
       console.log(result);
     } catch (E) {
@@ -49,12 +63,75 @@ export default function Profile() {
       const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
       if (status !== 'granted') {
         alert('Sorry, we need camera roll permissions to make this work!');
-      } 
+      }
       else {
         console.log(status);
       }
+
+
     }
   };
+
+
+
+  async function _handleImagePicked(pickerResult) {
+    let uploadResponse, uploadResult;
+
+    try {
+
+      if (!pickerResult.cancelled) {
+        uploadResponse = await uploadImageAsync(pickerResult.uri);
+        uploadResult = await uploadResponse.json();
+
+        setImage(uploadResult.location)
+      }
+    } catch (e) {
+      console.log({ uploadResponse });
+      console.log({ uploadResult });
+      console.log(e);
+      alert('Upload failed, sorry :(');
+    }
+  };
+
+
+  async function uploadImageAsync(uri) {
+    let apiUrl = 'http://192.168.15.13:3000/users';
+
+    // Note:
+    // Uncomment this if you want to experiment with local server
+    //
+    // if (Constants.isDevice) {
+    //   apiUrl = `https://your-ngrok-subdomain.ngrok.io/upload`;
+    // } else {
+    //   apiUrl = `http://localhost:3000/upload`
+    // }
+
+    let uriParts = uri.split('.');
+    let fileType = uriParts[uriParts.length - 1];
+
+    let formData = new FormData();
+
+    formData.append('userphoto', {
+      uri,
+      name: `photo.${fileType}`,
+      type: `image/${fileType}`,
+    });
+    formData.append('first_name', 'marlon')
+    formData.append('last_name', 'englemam')
+    formData.append('email', 'englemam@gmail.com')
+    formData.append('password', '333333')
+
+
+    let options = {
+      method: 'POST',
+      body: formData,
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    };
+
+    return await fetch(apiUrl, options);
+  }
 
   return (
     <View style={styles.container}>
