b974eb52622644c2c89286de44f88378f432be92
Eduardo Geraldini <eduardopr.geraldini@gmail.com>
2020-06-16 21:32:27 -0300

    card next consult

--- src/pages/Schedule/index.js
+++ src/pages/Schedule/index.js
@@ -35,6 +35,7 @@ export default function Schedule() {
         if (!data.message) {
           setConsultationsOpened(data.filter((consult) => consult.isOpen == 0));
         }
+
       } catch (error) {
         Alert.alert(
           "Ocorreu um erro!",
@@ -70,48 +71,62 @@ export default function Schedule() {
     <View style={styles.container}>
       <Text style={styles.title}>Próxima consulta</Text>
 
-      <View style={styles.nextSchedule}>
-        <View style={styles.headerNextSchedule}>
-          <Text style={styles.headerTitleNextSchedule}>
-            Consulta agendada para
-          </Text>
-          <Text style={styles.headerDateNextSchedule}>
-            25/04/2020 às 13:30h
-          </Text>
+      {consultationsOpened.length == 0 && (
+        <View style={styles.notFoundContainer}>
+          <Image
+            source={require("../../assets/not_found.png")}
+            resizeMode="contain"
+            style={{ width: 200, height: 200 }}
+          />
         </View>
-        <View style={styles.bodyNextSchedule}>
-          <View style={{ flexDirection: "row", alignItems: "center" }}>
-            <Image
-              source={{
-                uri: "https://api.adorable.io/avatars/285/5.png",
-              }}
-              style={styles.avatarBodyNextSchedule}
-            />
-            <View>
-              <Text style={styles.doctorNameBodyNextSchedule}>
-                Dr. Auzio da Silva
-              </Text>
-              <Text style={styles.doctorDescriptionBodyNextSchedule}>
-                Clinico Geral
-              </Text>
-            </View>
+      )}
+      {consultationsOpened.length > 0 && (
+        <View style={styles.nextSchedule}>
+          <View style={styles.headerNextSchedule}>
+            <Text style={styles.headerTitleNextSchedule}>
+              Consulta agendada para
+            </Text>
+            <Text style={styles.headerDateNextSchedule}>
+              {consultationsOpened[0].date} às {consultationsOpened[0].time}h
+            </Text>
           </View>
-          <TouchableOpacity onPress={() => callPhoneNmber("19994521874")}>
-            <Feather
-              style={styles.icons}
-              name="phone-call"
-              color="#4F46BA"
-              size={25}
-            />
+          <View style={styles.bodyNextSchedule}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Image
+                source={{
+                  uri: `${apiBase}${consultationsOpened[0].avatar_path}`,
+                }}
+                style={styles.avatarBodyNextSchedule}
+              />
+              <View>
+                <Text style={styles.doctorNameBodyNextSchedule}>
+                  {consultationsOpened[0].first_name}{" "}
+                  {consultationsOpened[0].last_name}
+                </Text>
+                <Text style={styles.doctorDescriptionBodyNextSchedule}>
+                  Clinico Geral
+                </Text>
+              </View>
+            </View>
+            <TouchableOpacity
+              onPress={() => callPhoneNmber(consultationsOpened[0].phone)}
+            >
+              <Feather
+                style={styles.icons}
+                name="phone-call"
+                color="#4F46BA"
+                size={25}
+              />
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity style={styles.footerNextSchedule}>
+            <Text style={styles.footerTextNextSchedule}>
+              Ver mais informações
+            </Text>
+            <Feather name="arrow-right" size={15} color="#4F46BA" />
           </TouchableOpacity>
         </View>
-        <TouchableOpacity style={styles.footerNextSchedule}>
-          <Text style={styles.footerTextNextSchedule}>
-            Ver mais informações
-          </Text>
-          <Feather name="arrow-right" size={15} color="#4F46BA" />
-        </TouchableOpacity>
-      </View>
+      )}
 
       <Text style={styles.title}>Consultas em aberto</Text>
 
@@ -120,7 +135,7 @@ export default function Schedule() {
           <Image
             source={require("../../assets/not_found.png")}
             resizeMode="contain"
-            style={{ width: 300, height: 300 }}
+            style={{ width: 200, height: 200 }}
           />
         </View>
       )}
